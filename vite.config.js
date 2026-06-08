48d8e89b21e30aca9878f45ff21463e83f7642e2
Yarlick1 <yaelcolin.ing@gmail.com>
2026-06-08 16:10:29 -0600

    Prueba de configuracion para netlify y gitpages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/tessy/'
+export default defineConfig(({ mode }) => {
+  // Si estamos en giHub Actions, esta variable existirá automáticamente
+  const isGitHubActions = process.env.GITHUB_ACTIONS === 'true'
+
+  return {
+    plugins: [react()],
+    // Si es GitHub, usa el subdirectorio. Si es Netlify (o local), usa la raíz.
+    base: isGitHubActions ? '/tessy/' : '/',
+  }
+
 })
