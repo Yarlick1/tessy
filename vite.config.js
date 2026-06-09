e2fd8849922e103608a1946521edbb6fbe5202c8
Yarlick1 <yaelcolin.ing@gmail.com>
2026-06-09 14:40:19 -0600

    Configuracion para netlify

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  // GitHub Actions define automáticamente la variable GITHUB_ACTIONS como 'true'
-  // Netlify define la variable NETLIFY como 'true'
-  const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
-
-  return {
-    plugins: [react()],
-    // Si es GitHub Pages, usa el nombre de tu repositorio. Si es Netlify (o local), usa la raíz.
-    base: isGitHubPages ? '/tessy/' : '/',
-    build: {
-      outDir: 'dist',
-    }
-  }
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/'
 })
